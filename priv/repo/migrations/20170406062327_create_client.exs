bc6ff7c3f1b557461a8377d3af2c989f9454171e
gmile <iamexile@gmail.com>
2017-04-06 15:13:59 +0200

    Initial migrations

---
+++ priv/repo/migrations/20170406062327_create_client.exs
@@ -0,0 +1,21 @@
+defmodule Trump.Repo.Migrations.CreateClient do
+  use Ecto.Migration
+
+  def change do
+    create table(:clients, primary_key: false) do
+      add :id, :uuid, primary_key: true
+      add :name, :string
+      add :secret, :string
+      add :redirect_uri, :string
+      add :settings, :jsonb
+      add :priv_settings, :jsonb
+      add :user_id, references(:users, on_delete: :delete_all, type: :uuid)
+
+      timestamps()
+    end
+
+    create index(:clients, [:user_id])
+    create unique_index(:clients, [:secret])
+    create unique_index(:clients, [:name])
+  end
+end
