751d6c7357f071b7daeeffc283d7dd1b77255659
gmile <iamexile@gmail.com>
2017-04-21 20:41:58 +0300

    Add configs

---
+++ config/authable.exs
@@ -0,0 +1,67 @@
+use Mix.Config
+
+config :shield,
+  confirmable: false,
+  otp_check: false,
+  hooks: Shield.Hook.Default,
+  views: %{
+    changeset: Shield.ChangesetView,
+    error: Shield.ErrorView,
+    app: Shield.AppView,
+    client: Shield.ClientView,
+    token: Shield.TokenView,
+    user: Shield.UserView
+  },
+  cors_origins: "http://localhost:4200, *",
+  front_end: %{
+    base: "http://localhost:4200",
+    confirmation_path: "/users/confirm?confirmation_token={{confirmation_token}}",
+    reset_password_path: "/users/reset-password?reset_token={{reset_token}}"
+  }
+
+config :authable,
+  ecto_repos: [Authable.Repo],
+  repo: Authable.Repo,
+  resource_owner: Authable.Model.User,
+  token_store: Authable.Model.Token,
+  client: Authable.Model.Client,
+  app: Authable.Model.App,
+  expires_in: %{
+    access_token: 3600,
+    refresh_token: 24 * 3600,
+    authorization_code: 300,
+    session_token: 30 * 24 * 3600
+  },
+  grant_types: %{
+    authorization_code: Authable.GrantType.AuthorizationCode,
+    client_credentials: Authable.GrantType.ClientCredentials,
+    password: Authable.GrantType.Password,
+    refresh_token: Authable.GrantType.RefreshToken
+  },
+  auth_strategies: %{
+    headers: %{
+      "authorization" => [
+        {~r/Basic ([a-zA-Z\-_\+=]+)/, Authable.Authentication.Basic},
+        {~r/Bearer ([a-zA-Z\-_\+=]+)/, Authable.Authentication.Bearer},
+      ],
+      "x-api-token" => [
+        {~r/([a-zA-Z\-_\+=]+)/, Authable.Authentication.Bearer}
+      ]
+    },
+    query_params: %{
+      "access_token" => Authable.Authentication.Bearer
+    },
+    sessions: %{
+      "session_token" => Authable.Authentication.Session
+    }
+  },
+  scopes: ~w(read write session),
+  renderer: Authable.Renderer.RestApi
+
+config :authable, Authable.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "trump_api_dev",
+  hostname: "localhost",
+  pool_size: 10
--- config/config.exs
+++ config/config.exs
@@ -61,4 +61,5 @@ config :trump_api, :generators,
   binary_id: true,
   sample_binary_id: "11111111-1111-1111-1111-111111111111"
 
+import_config "authable.exs"
 import_config "#{Mix.env}.exs"
