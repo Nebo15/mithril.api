4fd138b94bb557a8a111e08409f16aac073f0ecf
gmile <iamexile@gmail.com>
2017-04-21 22:09:24 +0300

    Update test script

--- test.fish
+++ test.fish
@@ -1,10 +1,11 @@
 set user_id (uuidgen)
 set user_email 'test@user.com'
 set user_password '$2b$12$p/rUfIGNhgnahKkjOedn/OeYWbVADwWwKWxLgq4Aa2kRTOYniR7Y'.
+set user_password_hash '$2b$12$p/rUfIGNhgnahKkjOedn/OeYWbVADwWwKWxLgq4Aa2kRTOYniR7Y'.
 
 # pass1234
 
 psql trump_api_dev -c "delete from users"
-psql trump_api_dev -c "insert into users (id, email, password, inserted_at, updated_at) values ('$user_id', '$user_email', '$user_password', now(), now())"
+psql trump_api_dev -c "insert into users (id, email, password, inserted_at, updated_at) values ('$user_id', '$user_email', '$user_password_hash', now(), now())"
 
-curl -X POST -H 'Content-Type: application/json' -d "{\"user\":{\"email\":\"$user_email\",\"password\":\"pass1234\"}}" 'http://localhost:4000/users/login'
+curl -X POST -H 'Content-Type: application/json' -d "{\"user\":{\"email\":\"$user_email\",\"password\":\"$user_password\"}}" 'http://localhost:4000/users/login'
