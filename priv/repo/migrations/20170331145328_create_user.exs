7c8183eeaf0497173a554fcdf6e143fe5115a544
gmile <iamexile@gmail.com>
2017-04-21 22:12:19 +0300

    Update user migration

--- priv/repo/migrations/20170331145328_create_user.exs
+++ priv/repo/migrations/20170331145328_create_user.exs
@@ -6,10 +6,12 @@ defmodule Trump.Repo.Migrations.CreateUser do
       add :id, :uuid, primary_key: true
       add :email, :string
       add :password, :string
-      add :scopes, {:array, :string}
+      add :settings, :jsonb
+      add :priv_settings, :jsonb
 
       timestamps()
     end
+    create unique_index(:users, [:email])
 
   end
 end
