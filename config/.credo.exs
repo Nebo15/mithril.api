325161631d9e5a1c9955163168bcdde4d7d09db9
gmile <iamexile@gmail.com>
2017-04-23 23:33:12 +0300

    Simplify credo

--- config/.credo.exs
+++ config/.credo.exs
@@ -8,10 +8,12 @@
         excluded: ["lib/trump_api/tasks.ex"]
       },
       checks: [
+        {Credo.Check.Design.AliasUsage, false},
         {Credo.Check.Design.TagTODO, exit_status: 0},
         {Credo.Check.Readability.MaxLineLength, priority: :low, max_length: 120},
         {Credo.Check.Readability.Specs, false},
-        {Credo.Check.Readability.ModuleDoc, false}
+        {Credo.Check.Readability.ModuleDoc, false},
+        {Credo.Check.Refactor.Nesting, false}
       ]
     }
   ]
