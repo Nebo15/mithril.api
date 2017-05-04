e66960b2283377ee816f496b3bc198a3b2596ae0
gmile <iamexile@gmail.com>
2017-05-04 15:18:32 +0300

    Remove autogenerated docs

--- lib/mithril_api/client_api/client_api.ex
+++ lib/mithril_api/client_api/client_api.ex
@@ -1,7 +1,5 @@
 defmodule Mithril.ClientAPI do
-  @moduledoc """
-  The boundary for the ClientAPI system.
-  """
+  @moduledoc false
 
   import Ecto.{Query, Changeset}, warn: false
   alias Mithril.Repo
@@ -9,47 +7,12 @@ defmodule Mithril.ClientAPI do
   alias Mithril.ClientAPI.Client
   alias Authable.Utils.Crypt, as: CryptUtil
 
-  @doc """
-  Returns the list of clients.
-
-  ## Examples
-
-      iex> list_clients()
-      [%Client{}, ...]
-
-  """
   def list_clients do
     Repo.all(Client)
   end
 
-  @doc """
-  Gets a single client.
-
-  Raises `Ecto.NoResultsError` if the Client does not exist.
-
-  ## Examples
-
-      iex> get_client!(123)
-      %Client{}
-
-      iex> get_client!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_client!(id), do: Repo.get!(Client, id)
 
-  @doc """
-  Creates a client.
-
-  ## Examples
-
-      iex> create_client(%{field: value})
-      {:ok, %Client{}}
-
-      iex> create_client(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_client(attrs \\ %{}) do
     changeset = client_changeset(%Client{}, attrs)
 
@@ -97,49 +60,16 @@ defmodule Mithril.ClientAPI do
     end
   end
 
-  @doc """
-  Updates a client.
-
-  ## Examples
-
-      iex> update_client(client, %{field: new_value})
-      {:ok, %Client{}}
-
-      iex> update_client(client, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_client(%Client{} = client, attrs) do
     client
     |> client_changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a Client.
-
-  ## Examples
-
-      iex> delete_client(client)
-      {:ok, %Client{}}
-
-      iex> delete_client(client)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_client(%Client{} = client) do
     Repo.delete(client)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking client changes.
-
-  ## Examples
-
-      iex> change_client(client)
-      %Ecto.Changeset{source: %Client{}}
-
-  """
   def change_client(%Client{} = client) do
     client_changeset(client, %{})
   end
