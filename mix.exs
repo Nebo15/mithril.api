83518f7763feaab1b691a56a00a44af78f17ff45
gmile <iamexile@gmail.com>
2017-05-12 15:02:41 +0300

    Make sure comeonin dependency is first-class

--- mix.exs
+++ mix.exs
@@ -61,6 +61,7 @@ defmodule Mithril.Mixfile do
      {:eview, "~> 0.10.7"},
      {:phoenix_ecto, "~> 3.2"},
      {:ecto_paging, ">= 0.0.0"},
+     {:comeonin, ">= 0.0.0"},
      {:authable, ">= 0.0.0", runtime: false},
      {:benchfella, ">= 0.3.4", only: [:dev, :test]},
      {:ex_doc, ">= 0.15.0", only: [:dev, :test]},
