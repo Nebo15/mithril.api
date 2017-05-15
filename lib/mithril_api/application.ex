4b720fb09c758be33364b89f1c8e376991165276
gmile <iamexile@gmail.com>
2017-05-15 07:32:09 +0300

    Add a few blan lines

--- lib/mithril_api/application.ex
+++ lib/mithril_api/application.ex
@@ -23,6 +23,9 @@ defmodule Mithril do
       # worker(Mithril.Worker, [arg1, arg2, arg3]),
     ]
 
+
+
+
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Mithril.Supervisor]
