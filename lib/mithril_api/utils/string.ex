4426c58a7c9934a27f559e1eee2ccaf619183c9f
gmile <iamexile@gmail.com>
2017-05-29 16:05:33 +0300

    Remove TODO

--- lib/mithril_api/utils/string.ex
+++ lib/mithril_api/utils/string.ex
@@ -1,6 +1,5 @@
 defmodule Mithril.Utils.String do
   @moduledoc false
 
-  # TODO: just use this directly, no need to have it module
   def comma_split(str), do: String.split(str, " ", trim: true)
 end
