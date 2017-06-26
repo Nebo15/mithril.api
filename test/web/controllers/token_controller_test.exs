82bd81573c7c2de5b3f8e9fa46e1d5a5e751e997
Max <max@Maxs-MacBook-Pro.local>
2017-06-26 19:15:14 +0300

    Added tests

--- test/web/controllers/token_controller_test.exs
+++ test/web/controllers/token_controller_test.exs
@@ -8,12 +8,13 @@ defmodule Mithril.Web.TokenControllerTest do
   @update_attrs %{details: %{}, expires_at: 43, name: "some updated name", value: "some updated value"}
   @invalid_attrs %{details: nil, expires_at: nil, name: nil, value: nil}
 
-  def fixture(:token, name \\ "some name") do
+  def fixture(:token, name \\ "some name", value \\ "some_value") do
     user = Mithril.Fixtures.create_user()
     {:ok, token} =
       @create_attrs
       |> Map.put_new(:user_id, user.id)
       |> Map.put(:name, name)
+      |> Map.put(:value, value)
       |> TokenAPI.create_token()
     token
   end
@@ -52,6 +53,30 @@ defmodule Mithril.Web.TokenControllerTest do
     token = fixture(:token, "3")
     conn = get conn, token_path(conn, :index), %{ending_before: token.id}
     assert 2 == length(json_response(conn, 200)["data"])
+  end
+
+  test "search by name by like works", %{conn: conn} do
+    fixture(:token, "refresh_token")
+    fixture(:token, "access_token")
+    fixture(:token, "something_different")
+    conn = get conn, token_path(conn, :index), %{name: "token"}
+    assert 2 == length(json_response(conn, 200)["data"])
+  end
+
+  test "search by value by like works", %{conn: conn} do
+    fixture(:token, "access_token", "111")
+    fixture(:token, "access_token", "123")
+    fixture(:token, "access_token", "234")
+    conn = get conn, token_path(conn, :index), %{value: "23"}
+    assert 2 == length(json_response(conn, 200)["data"])
+  end
+
+  test "search by user_id works", %{conn: conn} do
+    token = fixture(:token, "1")
+    fixture(:token, "2")
+    fixture(:token, "3")
+    conn = get conn, token_path(conn, :index), %{user_id: token.user_id}
+    assert 1 == length(json_response(conn, 200)["data"])
   end
 
   test "creates token and renders token when data is valid", %{conn: conn} do
